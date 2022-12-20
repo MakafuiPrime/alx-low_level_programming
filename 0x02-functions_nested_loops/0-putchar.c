e08550dda20188ae134adb930737222bd4ac3b91
Marthias Ahedor <marthiasahedor@gmail.com>
2022-12-19 22:24:20 -0800

    Loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+int _putchar(char c);
 /**
  *  * main -  prints _putchar, followed by a new line
  *   * Return: Always 0 (Success)
@@ -7,14 +7,14 @@
 int main(void)
 {
 		_putchar('_');
-			_putchar('p');
-				_putchar('u');
-					_putchar('t');
-						_putchar('c');
-							_putchar('h');
-								_putchar('a');
-									_putchar('r');
-										_putchar('\n');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
 
-											return (0);
+		return (0);
 }
